8a6f7c1c62ad87ee95467a42d6434bb13c156076
Pedro Nascimento <pedrodti2017@gmail.com>
2022-01-14 23:48:10 -0300

    função bebidas por pessoa

--- script.js
+++ script.js
@@ -11,7 +11,14 @@ function calcular(){
   let criancas = inputCriancas.value;
   let duracao = inputDuracao.value;
 
-  let quantidadeTotalCarne =  carnePessoa(duracao.value) * adultos + (carnePessoa(duracao) /2 * criancas); 
+  let quantidadeTotalCarne =  carnePessoa(duracao.value) * adultos + (carnePessoa(duracao) / 2 * criancas); 
+  let quantidadeTotalCerveja =  cervejaPessoa(duracao.value) * adultos; 
+  let quantidadeTotalBebidas =  bebidasPessoa(duracao.value) * adultos + (bebidasPessoa(duracao) / 2 * criancas); 
+
+
+  resultado.innerHTML = `<p> ${quantidadeTotalCarne}g de carne</p>`
+  resultado.innerHTML = `<p> ${quantidadeTotalCerveja}ml de cerveja</p>`
+  resultado.innerHTML = `<p> ${quantidadeTotalBebidas}ml de bebidas</p>`
 
   console.log(quantidadeTotalCarne)
 }
@@ -19,9 +26,24 @@ function calcular(){
 function carnePessoa(duracao){
   let carne = 400;
   if(duracao >= 6){
-    return 650
+    return 650;
   }else {
-    return 400
+    return 400;
   }
-  
+}
+
+function cervejaPessoa(duracao){
+  if(duracao >= 6){
+    return 2000;
+  }else {
+    return 1200;
+  }
+}
+
+function bebidasPessoa(duracao){
+  if(duracao >= 6 ){
+    return 1500;
+  } else {
+    return 1000;
+  }
 }
