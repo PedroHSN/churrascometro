51a1809dab4746c9e1a4d139718aba34b74594e0
Pedro Nascimento <pedrodti2017@gmail.com>
2022-01-14 23:36:28 -0300

    recebendo os dados, e resolvendo a quantidade de carne por pessoa

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-let inputAdultos = document.getElementById("adultos")
-let inputCriancas = document.getElementById("criancas")
-let inputDuracao = document.getElementById("duracao")
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular(){
+  console.log("calculado")
+
+  let adultos = inputAdultos.value;
+  let criancas = inputCriancas.value;
+  let duracao = inputDuracao.value;
+
+  let quantidadeTotalCarne =  carnePessoa(duracao.value) * adultos + (carnePessoa(duracao) /2 * criancas); 
+
+  console.log(quantidadeTotalCarne)
+}
+
+function carnePessoa(duracao){
+  let carne = 400;
+  if(duracao >= 6){
+    return 650
+  }else {
+    return 400
+  }
+  
+}
